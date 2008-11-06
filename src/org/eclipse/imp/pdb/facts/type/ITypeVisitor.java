581ae9f872a79c963d8684f39bced896013de252
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2008-11-06 14:19:57 +0000

    added copyright statement

--- src/org/eclipse/imp/pdb/facts/type/ITypeVisitor.java
+++ src/org/eclipse/imp/pdb/facts/type/ITypeVisitor.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+* Copyright (c) 2008 CWI.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   jurgen@vinju.org
+
+*******************************************************************************/
 package org.eclipse.imp.pdb.facts.type;
 
 
