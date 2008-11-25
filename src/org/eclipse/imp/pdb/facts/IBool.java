ee6c72a3a9d5abaacc3344b5f7b4fa14070b8dde
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2008-11-25 08:38:57 +0000

    added operations on booleans

--- src/org/eclipse/imp/pdb/facts/IBool.java
+++ src/org/eclipse/imp/pdb/facts/IBool.java
@@ -2,4 +2,10 @@ package org.eclipse.imp.pdb.facts;
 
 public interface IBool extends IValue {
 	boolean getValue();
+	IBool and(IBool other);
+	IBool or(IBool other);
+	IBool xor(IBool other);
+	IBool not();
+	IBool implies(IBool other);
+	IBool equivalent(IBool other);
 }
--- src/org/eclipse/imp/pdb/facts/impl/BoolValue.java
+++ src/org/eclipse/imp/pdb/facts/impl/BoolValue.java
@@ -18,7 +18,7 @@ public class BoolValue extends Value implements IBool {
 		super(other, label, anno);
     	fValue = other.fValue;
 	}
-
+	
 	@Override
 	protected IValue clone(String label, IValue value) {
 		return new BoolValue(this, label, value);
@@ -43,6 +43,31 @@ public class BoolValue extends Value implements IBool {
 			return fValue == ((BoolValue) obj).fValue;
 		}
 		return false;
+	}
+	
+	public IBool and(IBool other) {
+		return new BoolValue(fValue && other.getValue());
+	}
+	
+	public IBool or(IBool other) {
+		return new BoolValue(fValue || other.getValue());
+	}
+	
+	public IBool not() {
+		return new BoolValue(!fValue);
+	}
+	
+	public IBool implies(IBool other) {
+		return new BoolValue(fValue ? other.getValue() : true);
+	}
+	
+	public IBool equivalent(IBool other) {
+		return new BoolValue(fValue == other.getValue());
+	}
+	
+	public IBool xor(IBool other) {
+		boolean otherValue = other.getValue();
+		return new BoolValue((fValue || otherValue) && !(fValue && otherValue));
 	}
 	
 	@Override
