e471ba66514630ace79e164752749ba266b69d98
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2008-12-23 08:17:55 +0000

    added ATermReader and empty ATermWriter (CQ 2908 is now accepted for parallel IP, Bug 259217 is now half solved)

---
+++ src/org/eclipse/imp/pdb/facts/io/ATermWriter.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+* Copyright (c) CWI 2008 
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Jurgen Vinju (jurgenv@cwi.nl) - initial API and implementation
+
+*******************************************************************************/
+package org.eclipse.imp.pdb.facts.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.eclipse.imp.pdb.facts.IValue;
+
+public class ATermWriter implements IValueWriter {
+
+	public void write(IValue value, OutputStream stream) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
