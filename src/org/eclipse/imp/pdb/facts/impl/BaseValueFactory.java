1a527fe796b9eed70720fba7d4a4d53ed8187de9
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2009-03-09 11:58:45 +0000

    added more null checks

--- src/org/eclipse/imp/pdb/facts/impl/BaseValueFactory.java
+++ src/org/eclipse/imp/pdb/facts/impl/BaseValueFactory.java
@@ -31,10 +31,19 @@ public abstract class BaseValueFactory implements IValueFactory {
     }
     
     public IString string(String s) {
+    	if (s == null) {
+    		throw new NullPointerException();
+    	}
         return new StringValue(s);
     }
     
     public ISourceLocation sourceLocation(URL path, int startOffset, int length, int startLine, int endLine, int startCol, int endCol) {
+    	if (path == null) {
+    		throw new NullPointerException();
+    	}
+    	if (startOffset < 0 || length < 0 || startLine < 0 || endLine < startLine || endCol < 0 || (startLine == endLine && endCol < startCol)) {
+    		throw new IllegalArgumentException();
+    	}
         return new SourceLocationValue(path, startOffset, length, startLine, endLine, startCol, endCol);
     }
     
