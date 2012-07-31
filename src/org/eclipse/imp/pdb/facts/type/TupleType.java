589af5790b266cb49ad9c6c9a8482a2774cd9a26
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2012-07-31 20:21:35 +0000

    fixed efficiency hog, misusing an exception to indicate a boolean is not a good plan. thanks to Anya Bagge for pointing it out.

--- src/org/eclipse/imp/pdb/facts/type/TupleType.java
+++ src/org/eclipse/imp/pdb/facts/type/TupleType.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 
-*******************************************************************************/
+ *******************************************************************************/
 
 package org.eclipse.imp.pdb.facts.type;
 
@@ -21,129 +21,132 @@ import org.eclipse.imp.pdb.facts.exceptions.FactTypeUseException;
 import org.eclipse.imp.pdb.facts.exceptions.IllegalOperationException;
 import org.eclipse.imp.pdb.facts.exceptions.UndeclaredFieldException;
 
-/*package*/ final class TupleType extends Type {
+/*package*/final class TupleType extends Type {
 	private final Type[] fFieldTypes;
 	private final String[] fFieldNames;
-	private int fHashcode= -1;
+	private int fHashcode = -1;
 
-    /**
-     * Creates a tuple type with the given field types. Copies the array.
-     */
-    /*package*/ TupleType(Type[] fieldTypes){
-    	super();
-    	
-        fFieldTypes = fieldTypes; // fieldTypes.clone(); was safer, but it ended up being a bottleneck
-        fFieldNames = null;
-    }
+	/**
+	 * Creates a tuple type with the given field types. Copies the array.
+	 */
+	/* package */TupleType(Type[] fieldTypes) {
+		super();
 
-    /**
-     * Creates a tuple type with the given field types and names. Copies the arrays.
-     */
-    /*package*/ TupleType(Type[] fieldTypes, String[] fieldNames){
-    	super();
-    	
-    	fFieldTypes = fieldTypes; // fieldTypes.clone(); was safer, but it ended up being a bottleneck
-		if(fieldNames.length != 0) {
+		fFieldTypes = fieldTypes; // fieldTypes.clone(); was safer, but it ended
+									// up being a bottleneck
+		fFieldNames = null;
+	}
+
+	/**
+	 * Creates a tuple type with the given field types and names. Copies the
+	 * arrays.
+	 */
+	/* package */TupleType(Type[] fieldTypes, String[] fieldNames) {
+		super();
+
+		fFieldTypes = fieldTypes; // fieldTypes.clone(); was safer, but it ended
+									// up being a bottleneck
+		if (fieldNames.length != 0) {
 			fFieldNames = fieldNames; // fieldNames.clone(); same here
+		} else {
+			fFieldNames = null;
 		}
-		else fFieldNames = null;
-    }
-    
-    @Override
-    public boolean hasFieldNames() {
-    	return fFieldNames != null;
-    }
-    
-    @Override
-    public boolean isTupleType() {
-    	return true;
-    }
-    
-    @Override
-    public Type getFieldType(int i) {
-        return fFieldTypes[i];
-    }
+	}
 
-    @Override
-    public Type getFieldType(String fieldName) {
-    	return getFieldType(getFieldIndex(fieldName));
-    }
-    
-    @Override
-    public int getFieldIndex(String fieldName) throws FactTypeUseException {
-    	if (fFieldNames != null) {
-    		for (int i = fFieldNames.length - 1; i >= 0; i--) {
-    			if (fFieldNames[i].equals(fieldName)) {
-    				return i;
-    			}
-    		}
-    	}
+	@Override
+	public boolean hasFieldNames() {
+		return fFieldNames != null;
+	}
+
+	@Override
+	public boolean isTupleType() {
+		return true;
+	}
+
+	@Override
+	public Type getFieldType(int i) {
+		return fFieldTypes[i];
+	}
+
+	@Override
+	public Type getFieldType(String fieldName) {
+		return getFieldType(getFieldIndex(fieldName));
+	}
+
+	@Override
+	public int getFieldIndex(String fieldName) throws FactTypeUseException {
+		if (fFieldNames != null) {
+			for (int i = fFieldNames.length - 1; i >= 0; i--) {
+				if (fFieldNames[i].equals(fieldName)) {
+					return i;
+				}
+			}
+		}
 
 		throw new UndeclaredFieldException(this, fieldName);
-    }
-    
-    @Override
+	}
+
+	@Override
     public boolean hasField(String fieldName) {
-    	try {
-    		return getFieldIndex(fieldName) != -1;
-    	}
-    	catch (FactTypeUseException e) {
-    		return false;
-    	}
-    }
-    
-    @Override
-    public int getArity() {
-        return fFieldTypes.length;
-    }
-    
-    @Override
-    public Type compose(Type other) {
-    	if (other.isVoidType()) {
-    		return other;
-    	}
-    	
-    	if (this.getArity() != 2 || other.getArity() != 2) {
-			throw new IllegalOperationException("compose", this, other);
-		}
-		
-		if (!getFieldType(1).comparable(other.getFieldType(0))) {
-			throw new IllegalOperationException("compose", this, other);
-		}
-		
-		TypeFactory tf = TypeFactory.getInstance();
-		
-		if (hasFieldNames() && other.hasFieldNames()) {
-			String fieldNameLeft = this.getFieldName(0);
-			String fieldNameRight = other.getFieldName(1);
-			
-			if (!fieldNameLeft.equals(fieldNameRight)) {
-				return tf.tupleType(
-						this.getFieldType(0), 
-						fieldNameLeft,
-						other.getFieldType(1), 
-						fieldNameRight
-				);
+		if (fFieldNames != null) {
+			for (int i = fFieldNames.length - 1; i >= 0; i--) {
+				if (fFieldNames[i].equals(fieldName)) {
+					return true;
+				}
 			}
 		}
 		
-		return tf.tupleType(this.getFieldType(0), other.getFieldType(1));
-    }
-    
-    @Override
-    public Type carrier() {
-    	Type lub = TypeFactory.getInstance().voidType();
-    	
-    	for (Type field : this) {
-    		lub = lub.lub(field);
+    		return false;
     	}
-    	
-    	return TypeFactory.getInstance().setType(lub);
-    }
 
-    @Override
-    public boolean isSubtypeOf(Type o) {
-    	if (o == this) {
+	@Override
+	public int getArity() {
+		return fFieldTypes.length;
+	}
+
+	@Override
+	public Type compose(Type other) {
+		if (other.isVoidType()) {
+			return other;
+		}
+
+		if (this.getArity() != 2 || other.getArity() != 2) {
+			throw new IllegalOperationException("compose", this, other);
+		}
+
+		if (!getFieldType(1).comparable(other.getFieldType(0))) {
+			throw new IllegalOperationException("compose", this, other);
+		}
+
+		TypeFactory tf = TypeFactory.getInstance();
+
+		if (hasFieldNames() && other.hasFieldNames()) {
+			String fieldNameLeft = this.getFieldName(0);
+			String fieldNameRight = other.getFieldName(1);
+
+			if (!fieldNameLeft.equals(fieldNameRight)) {
+				return tf.tupleType(this.getFieldType(0), fieldNameLeft,
+						other.getFieldType(1), fieldNameRight);
+			}
+		}
+
+		return tf.tupleType(this.getFieldType(0), other.getFieldType(1));
+	}
+
+	@Override
+	public Type carrier() {
+		Type lub = TypeFactory.getInstance().voidType();
+
+		for (Type field : this) {
+			lub = lub.lub(field);
+		}
+
+		return TypeFactory.getInstance().setType(lub);
+	}
+
+	@Override
+	public boolean isSubtypeOf(Type o) {
+		if (o == this) {
 			return true; // optimize to prevent loop
 		} else if (o.isTupleType()) {
 			if (getArity() == o.getArity()) {
@@ -157,168 +160,167 @@ import org.eclipse.imp.pdb.facts.exceptions.UndeclaredFieldException;
 		}
 
 		return super.isSubtypeOf(o);
-    }
+	}
 
-    /**
-     * Compute a new tupletype that is the lub of t1 and t2. 
-     * Precondition: t1 and t2 have the same arity.
-     * @param t1
-     * @param t2
-     * @return a TupleType which is the lub of t1 and t2
-     */
-    private Type lubTupleTypes(Type t1, Type t2) {
-    	int N = t1.getArity();
-    	Type[] fieldTypes = new Type[N];
-    	String[] fieldNames = new String[N];
-    	
-    	for (int i = 0; i < N; i++) {
-    		fieldTypes[i] = t1.getFieldType(i).lub(t2.getFieldType(i));
-    		
-    		if (t1.hasFieldNames()) {
-    			fieldNames[i] = t1.getFieldName(i);
-    		}
-    		else if (t2.hasFieldNames()) {
-    			fieldNames[i] = t2.getFieldName(i);
-    		}
-    	}
-    	
-    	return TypeFactory.getInstance().tupleType(fieldTypes);
-    }
-    
-    /**
-     * Compute a new tupletype that is the lub of t1 and t2. 
-     * Precondition: t1 and t2 have the same arity.
-     * @param t1
-     * @param t2
-     * @return a TupleType which is the lub of t1 and t2, if all the names are equal at every 
-     *         position, they remain, otherwise we get an unlabeled tuple.
-     */
-    private Type lubNamedTupleTypes(Type t1, Type t2) {
-    	int N = t1.getArity();
-    	Object[] fieldTypes = new Object[N*2];
-    	Type[] types = new Type[N];
-    	boolean first = t1.hasFieldNames();
-    	boolean second = t2.hasFieldNames();
-    	boolean consistent = true;
-    	
-    	for (int i = 0, j = 0; i < N; i++, j++) {
-    		Type lub = t1.getFieldType(i).lub(t2.getFieldType(i));
-    		types[i] = lub;
+	/**
+	 * Compute a new tupletype that is the lub of t1 and t2. Precondition: t1
+	 * and t2 have the same arity.
+	 * 
+	 * @param t1
+	 * @param t2
+	 * @return a TupleType which is the lub of t1 and t2
+	 */
+	private Type lubTupleTypes(Type t1, Type t2) {
+		int N = t1.getArity();
+		Type[] fieldTypes = new Type[N];
+		String[] fieldNames = new String[N];
+
+		for (int i = 0; i < N; i++) {
+			fieldTypes[i] = t1.getFieldType(i).lub(t2.getFieldType(i));
+
+			if (t1.hasFieldNames()) {
+				fieldNames[i] = t1.getFieldName(i);
+			} else if (t2.hasFieldNames()) {
+				fieldNames[i] = t2.getFieldName(i);
+			}
+		}
+
+		return TypeFactory.getInstance().tupleType(fieldTypes);
+	}
+
+	/**
+	 * Compute a new tupletype that is the lub of t1 and t2. Precondition: t1
+	 * and t2 have the same arity.
+	 * 
+	 * @param t1
+	 * @param t2
+	 * @return a TupleType which is the lub of t1 and t2, if all the names are
+	 *         equal at every position, they remain, otherwise we get an
+	 *         unlabeled tuple.
+	 */
+	private Type lubNamedTupleTypes(Type t1, Type t2) {
+		int N = t1.getArity();
+		Object[] fieldTypes = new Object[N * 2];
+		Type[] types = new Type[N];
+		boolean first = t1.hasFieldNames();
+		boolean second = t2.hasFieldNames();
+		boolean consistent = true;
+
+		for (int i = 0, j = 0; i < N; i++, j++) {
+			Type lub = t1.getFieldType(i).lub(t2.getFieldType(i));
+			types[i] = lub;
 			fieldTypes[j++] = lub;
-    		
+
 			if (first && second) {
 				String fieldName1 = t1.getFieldName(i);
 				String fieldName2 = t2.getFieldName(i);
-				
+
 				if (fieldName1.equals(fieldName2)) {
 					fieldTypes[j] = fieldName1;
-				}
-				else {
+				} else {
 					consistent = false;
 				}
-    		}
-    		else if (first) {
-    			fieldTypes[j] = t1.getFieldName(i);
-    		}
-    		else if (second) {
-    			fieldTypes[i] = t2.getFieldName(i);
-    		}
-    	}
-    	
-    	if (consistent && first && second) {
-    		return TypeFactory.getInstance().tupleType(fieldTypes);
-    	}
-    	else {
-    		return TypeFactory.getInstance().tupleType(types);
-    	}
-    }
-    
-    @Override
-    public Type lub(Type o) {
-    	if (o.isTupleType()) {
-    		if (getArity() == o.getArity()) {
-    			if (hasFieldNames() || o.hasFieldNames()) {
-    				return lubNamedTupleTypes(this, o);
-    			}
-    			
-    			return lubTupleTypes(this, o);
-    		}
-    	}
-    	return super.lub(o);
-    }
+			} else if (first) {
+				fieldTypes[j] = t1.getFieldName(i);
+			} else if (second) {
+				fieldTypes[i] = t2.getFieldName(i);
+			}
+		}
 
-    @Override
-    public int hashCode() {
-    	int h = fHashcode;
-        if (h == -1) {
-            h = 55501;
-            for(Type elemType: fFieldTypes) {
-                h = h * 44927 + elemType.hashCode();
-            }
-            fHashcode = h;
-        }
-        return h;
-    }
+		if (consistent && first && second) {
+			return TypeFactory.getInstance().tupleType(fieldTypes);
+		} else {
+			return TypeFactory.getInstance().tupleType(types);
+		}
+	}
 
-    /**
-     * Compute tuple type equality. Note that field labels are significant here
-     * for equality while they do not count for isSubtypeOf and lub.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof TupleType)) {
-            return false;
-        }
-        
-        TupleType other= (TupleType) obj;
-        if (fFieldTypes.length != other.fFieldTypes.length) {
-            return false;
-        }
-        
-        for(int i = fFieldTypes.length - 1; i >= 0; i--) {
-            // N.B.: The field types must have been created and canonicalized before any
-            // attempt to manipulate the outer type (i.e. TupleType), so we can use object
-            // identity here for the fFieldTypes.
-            if (fFieldTypes[i] != other.fFieldTypes[i]) {
-                return false;
-            }
-        }
-        
-        if (fFieldNames != null) {
-        	if (other.fFieldNames == null) {
-        		return false;
-        	}
-        	for (int i = fFieldNames.length - 1; i >= 0; i--) {
-              if (!fFieldNames[i].equals(other.fFieldNames[i])) {
-            	  return false;
-              }
-        	}
-        }
-        else if (other.fFieldNames != null) {
-        	return false;
-        }
-        
-        return true;
-    }
+	@Override
+	public Type lub(Type o) {
+		if (o.isTupleType()) {
+			if (getArity() == o.getArity()) {
+				if (hasFieldNames() || o.hasFieldNames()) {
+					return lubNamedTupleTypes(this, o);
+				}
 
-    @Override
-    public String toString() {
-        StringBuilder sb= new StringBuilder();
-        sb.append("tuple[");
-        int idx= 0;
-        for(Type elemType: fFieldTypes) {
-            if (idx++ > 0)
-                sb.append(",");
-            sb.append(elemType.toString());
-            if (hasFieldNames()) {
-            	sb.append(" " + fFieldNames[idx-1]);
-            }
-        }
-        sb.append("]");
-        return sb.toString();
-    }
+				return lubTupleTypes(this, o);
+			}
+		}
+		return super.lub(o);
+	}
 
-    @Override
+	@Override
+	public int hashCode() {
+		int h = fHashcode;
+		if (h == -1) {
+			h = 55501;
+			for (Type elemType : fFieldTypes) {
+				h = h * 44927 + elemType.hashCode();
+			}
+			fHashcode = h;
+		}
+		return h;
+	}
+
+	/**
+	 * Compute tuple type equality. Note that field labels are significant here
+	 * for equality while they do not count for isSubtypeOf and lub.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof TupleType)) {
+			return false;
+		}
+
+		TupleType other = (TupleType) obj;
+		if (fFieldTypes.length != other.fFieldTypes.length) {
+			return false;
+		}
+
+		for (int i = fFieldTypes.length - 1; i >= 0; i--) {
+			// N.B.: The field types must have been created and canonicalized
+			// before any
+			// attempt to manipulate the outer type (i.e. TupleType), so we can
+			// use object
+			// identity here for the fFieldTypes.
+			if (fFieldTypes[i] != other.fFieldTypes[i]) {
+				return false;
+			}
+		}
+
+		if (fFieldNames != null) {
+			if (other.fFieldNames == null) {
+				return false;
+			}
+			for (int i = fFieldNames.length - 1; i >= 0; i--) {
+				if (!fFieldNames[i].equals(other.fFieldNames[i])) {
+					return false;
+				}
+			}
+		} else if (other.fFieldNames != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("tuple[");
+		int idx = 0;
+		for (Type elemType : fFieldTypes) {
+			if (idx++ > 0)
+				sb.append(",");
+			sb.append(elemType.toString());
+			if (hasFieldNames()) {
+				sb.append(" " + fFieldNames[idx - 1]);
+			}
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+	@Override
 	public Iterator<Type> iterator() {
 		return new Iterator<Type>() {
 			private int cursor = 0;
@@ -336,43 +338,43 @@ import org.eclipse.imp.pdb.facts.exceptions.UndeclaredFieldException;
 			}
 		};
 	}
-	
+
 	@Override
 	public <T> T accept(ITypeVisitor<T> visitor) {
 		return visitor.visitTuple(this);
 	}
 
-	 @Override
+	@Override
 	public IValue make(IValueFactory f) {
 		return f.tuple();
 	}
 
-	 @Override
+	@Override
 	public IValue make(IValueFactory f, IValue... elems) {
 		return f.tuple(elems);
 	}
 
-	 @Override
+	@Override
 	public String getFieldName(int i) {
 		return fFieldNames != null ? fFieldNames[i] : null;
 	}
-	
+
 	@Override
 	public boolean match(Type matched, Map<Type, Type> bindings)
 			throws FactTypeUseException {
 		if (!super.match(matched, bindings)) {
 			return false;
 		}
-		
+
 		for (int i = getArity() - 1; i >= 0; i--) {
 			if (!getFieldType(i).match(matched.getFieldType(i), bindings)) {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	@Override
 	public Type instantiate(Map<Type, Type> bindings) {
 		if (hasFieldNames()) {
@@ -386,7 +388,7 @@ import org.eclipse.imp.pdb.facts.exceptions.UndeclaredFieldException;
 
 			return TypeFactory.getInstance().tupleType(fTypes, fLabels);
 		}
-		
+
 		Type[] fChildren = new Type[getArity()];
 		for (int i = fChildren.length - 1; i >= 0; i--) {
 			fChildren[i] = getFieldType(i).instantiate(bindings);
@@ -395,58 +397,56 @@ import org.eclipse.imp.pdb.facts.exceptions.UndeclaredFieldException;
 		return TypeFactory.getInstance().tupleType(fChildren);
 	}
 
-	 @Override
+	@Override
 	public Type select(int... fields) {
-        int width = fields.length;
-        
-        if (width == 0) {
-        	return TypeFactory.getInstance().voidType();
-        }
-        else if (width == 1) {
-        	return getFieldType(fields[0]);
-        }
-        else {
-        	if (!hasFieldNames()) {
-        		Type[] fieldTypes = new Type[width];
-    		    for (int i = width - 1; i >= 0; i--) {
-    		    	fieldTypes[i] = getFieldType(fields[i]);
-    		    }
-    		    
-    		    return TypeFactory.getInstance().tupleType(fieldTypes);
-        	}
-        	
-    		Type[] fieldTypes = new Type[width];
-    		String[] fieldNames = new String[width];
-    		boolean seenDuplicate = false;
-    		
-		    for (int i = width - 1; i >= 0; i--) {
-		    	fieldTypes[i] = getFieldType(fields[i]);
-		    	fieldNames[i] = getFieldName(fields[i]);
-		    	
-		    	for (int j = width - 1; j > i; j--) {
-		    		if (fieldNames[j].equals(fieldNames[i])) {
-		    			seenDuplicate = true;
-		    		}
-		    	}
-		    }
-		    
-		    if (!seenDuplicate) {
-		    	return TypeFactory.getInstance().tupleType(fieldTypes, fieldNames);
-		    }
-		    else {
-		    	return TypeFactory.getInstance().tupleType(fieldTypes);
-		    }
-        }
+		int width = fields.length;
+
+		if (width == 0) {
+			return TypeFactory.getInstance().voidType();
+		} else if (width == 1) {
+			return getFieldType(fields[0]);
+		} else {
+			if (!hasFieldNames()) {
+				Type[] fieldTypes = new Type[width];
+				for (int i = width - 1; i >= 0; i--) {
+					fieldTypes[i] = getFieldType(fields[i]);
+				}
+
+				return TypeFactory.getInstance().tupleType(fieldTypes);
+			}
+
+			Type[] fieldTypes = new Type[width];
+			String[] fieldNames = new String[width];
+			boolean seenDuplicate = false;
+
+			for (int i = width - 1; i >= 0; i--) {
+				fieldTypes[i] = getFieldType(fields[i]);
+				fieldNames[i] = getFieldName(fields[i]);
+
+				for (int j = width - 1; j > i; j--) {
+					if (fieldNames[j].equals(fieldNames[i])) {
+						seenDuplicate = true;
+					}
+				}
+			}
+
+			if (!seenDuplicate) {
+				return TypeFactory.getInstance().tupleType(fieldTypes,
+						fieldNames);
+			} else {
+				return TypeFactory.getInstance().tupleType(fieldTypes);
+			}
+		}
 	}
-	
-	 @Override
+
+	@Override
 	public Type select(String... names) {
 		int[] indexes = new int[names.length];
 		int i = 0;
 		for (String name : names) {
 			indexes[i] = getFieldIndex(name);
 		}
-		
+
 		return select(indexes);
 	}
 }
