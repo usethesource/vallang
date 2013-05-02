38db718c11fc5068aa8e40e43eba429b438fa468
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-05-02 14:39:00 +0200

    Correcting script which adds non-maven dependency.

--- add-cofoja.sh
+++ add-cofoja.sh
@@ -1,2 +1,2 @@
-mvn install:install-file -Dfile=lib/cofoja-1.1-r146.jar -DgroupId=com.google.java.contract \
+mvn install:install-file -Dfile=lib/cofoja-1.1-SNAPSHOT.jar -DgroupId=com.google.java.contract \
     -DartifactId=cofoja -Dversion=1.1-SNAPSHOT -Dpackaging=jar
