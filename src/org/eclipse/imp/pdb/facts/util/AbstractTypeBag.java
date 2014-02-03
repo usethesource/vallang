a81e7f0c08d4ea5483edc437f07d8616fcd358ed
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2014-02-03 21:43:23 +0100

    Adding a type bag for calculating the least upper bound of a collection.

---
+++ src/org/eclipse/imp/pdb/facts/util/AbstractTypeBag.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2014 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *
+ *   * Michael Steindorfer - Michael.Steindorfer@cwi.nl - CWI  
+ *******************************************************************************/
+package org.eclipse.imp.pdb.facts.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.imp.pdb.facts.type.Type;
+import org.eclipse.imp.pdb.facts.type.TypeFactory;
+
+public abstract class AbstractTypeBag implements Cloneable {
+		
+	public abstract void increase(Type t);
+
+	public abstract void decrease(Type t);
+	
+	public abstract Type lub();
+	
+	public abstract String getLabel();
+
+	public abstract AbstractTypeBag clone();
+	
+	public static AbstractTypeBag of(Type... ts) {
+		return of(null, ts);
+	}
+	
+	public static AbstractTypeBag of(String label, Type... ts) {
+		return new TypeBag(label, ts);
+	}
+	
+	private static class TypeBag extends AbstractTypeBag {
+		private final String label;
+		private final Map<Type, Integer> countMap; 
+		
+		private Type cachedLub = null;
+
+		private TypeBag(String label, Map<Type, Integer> countMap) {
+			this.label = label;
+			this.countMap = new HashMap<>(countMap);
+		}
+		
+		private TypeBag(Type... ts) {
+			this(null, ts);
+		}
+		
+		private TypeBag(String label, Type... ts) {
+			this.label = label;
+			this.countMap = new HashMap<Type, Integer>(4);
+			
+			for (Type t : ts) {
+				this.increase(t);
+			}
+		}
+		
+		@Override
+		public void increase(Type t) {
+			final Integer oldCount = countMap.get(t);
+			if (oldCount == null) {
+				countMap.put(t, 1);
+			} else {
+				countMap.put(t, oldCount + 1);
+			}
+		}
+
+		@Override
+		public void decrease(Type t) {
+			final Integer oldCount = countMap.remove(t);
+			if (oldCount == null) {
+				throw new IllegalStateException(String.format("Type '%s' was not present.", t));
+			} else if (oldCount > 0) {
+				countMap.put(t, oldCount - 1);
+			} else {
+				// count was zero, thus do not reinsert
+			}
+		}
+		
+		@Override
+		public Type lub() {
+			if (cachedLub == null) {			
+				Type inferredLubType = TypeFactory.getInstance().voidType();
+				for (Type t : countMap.keySet()) {
+					inferredLubType = inferredLubType.lub(t);
+				}				
+				cachedLub = inferredLubType;
+			}
+			return cachedLub;
+		}	
+
+		@Override
+		public String getLabel() {
+			return label;
+		}
+
+		@Override
+		public AbstractTypeBag clone() {
+			return new TypeBag(label, countMap);
+		}
+	}
+	
+}
