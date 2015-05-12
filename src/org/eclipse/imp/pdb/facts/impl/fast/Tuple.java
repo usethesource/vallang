d5346eee17f3e4662a70330b71161b46412dcd8b
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2015-05-12 14:09:42 +0200

    Formatting source of Tuple type.

--- src/org/eclipse/imp/pdb/facts/impl/fast/Tuple.java
+++ src/org/eclipse/imp/pdb/facts/impl/fast/Tuple.java
@@ -25,195 +25,207 @@ import org.eclipse.imp.pdb.facts.visitors.IValueVisitor;
  * 
  * @author Arnold Lankamp
  */
-/*package*/ public class Tuple extends AbstractValue implements ITuple{
-	protected final static TypeFactory typeFactory = TypeFactory.getInstance();
+public class Tuple extends AbstractValue implements ITuple {
 	
+	protected final static TypeFactory typeFactory = TypeFactory.getInstance();
+
 	protected final Type tupleType;
 	protected final IValue[] elements;
-	
-	/*package*/ public static ITuple newTuple(Type tupleType, IValue[] elements) {
+
+	public static ITuple newTuple(Type tupleType, IValue[] elements) {
 		return new Tuple(tupleType, elements);
 	}
-	
+
 	private Tuple(Type tupleType, IValue[] elements) {
 		super();
-		
+
 		this.tupleType = tupleType;
-		
+
 		this.elements = elements;
 	}
-	
-	/*package*/ public static ITuple newTuple(IValue... elements) {
+
+	public static ITuple newTuple(IValue... elements) {
 		return new Tuple(elements);
 	}
-	
-	private Tuple(IValue... elements) {
-	    super();
-	    this.tupleType = TypeFactory.getInstance().tupleType(elements);
-		this.elements= elements;
-	    }
 
-	public Type getType(){
+	private Tuple(IValue... elements) {
+		super();
+		this.tupleType = TypeFactory.getInstance().tupleType(elements);
+		this.elements = elements;
+	}
+
+	public Type getType() {
 		return tupleType;
 	}
 
-	public int arity(){
+	public int arity() {
 		return elements.length;
 	}
 
-	public IValue get(int i){
+	public IValue get(int i) {
 		return elements[i];
 	}
 
-	public IValue get(String label){
+	public IValue get(String label) {
 		return elements[tupleType.getFieldIndex(label)];
 	}
-	
-	public Iterator<IValue> iterator(){
+
+	public Iterator<IValue> iterator() {
 		return new TupleIterator(this);
 	}
-	
-	public <T, E extends Throwable> T accept(IValueVisitor<T,E> v) throws E{
+
+	public <T, E extends Throwable> T accept(IValueVisitor<T, E> v) throws E {
 		return v.visitTuple(this);
 	}
 
-	public ITuple set(int index, IValue arg){
+	public ITuple set(int index, IValue arg) {
 		int nrOfElements = elements.length;
 		IValue[] newElements = new IValue[nrOfElements];
 		Type[] elementTypes = new Type[nrOfElements];
-		for(int i = nrOfElements - 1; i >= 0; i--){
+		for (int i = nrOfElements - 1; i >= 0; i--) {
 			IValue element = elements[i];
 			newElements[i] = element;
 			elementTypes[i] = element.getType();
 		}
-		
+
 		newElements[index] = arg;
 		elementTypes[index] = arg.getType();
-		
+
 		return new Tuple(typeFactory.tupleType(elementTypes), newElements);
 	}
 
-	public ITuple set(String label, IValue arg){
+	public ITuple set(String label, IValue arg) {
 		int nrOfElements = elements.length;
 		IValue[] newElements = new IValue[nrOfElements];
 		Type[] elementTypes = new Type[nrOfElements];
-		for(int i = nrOfElements - 1; i >= 0; i--){
+		for (int i = nrOfElements - 1; i >= 0; i--) {
 			IValue element = elements[i];
 			newElements[i] = element;
 			elementTypes[i] = element.getType();
 		}
-		
+
 		newElements[tupleType.getFieldIndex(label)] = arg;
 		elementTypes[tupleType.getFieldIndex(label)] = arg.getType();
-		
+
 		return new Tuple(typeFactory.tupleType(elementTypes), newElements);
 	}
 
-	public IValue select(int... indexes){
-		if(indexes.length == 1) return get(indexes[0]);
-		
+	public IValue select(int... indexes) {
+		if (indexes.length == 1)
+			return get(indexes[0]);
+
 		int nrOfElements = indexes.length;
 		IValue[] elements = new IValue[nrOfElements];
 		Type[] elementTypes = new Type[nrOfElements];
-		for(int i = nrOfElements - 1; i >= 0 ; i--){
+		for (int i = nrOfElements - 1; i >= 0; i--) {
 			IValue element = get(indexes[i]);
 			elements[i] = element;
 			elementTypes[i] = element.getType();
 		}
-		
+
 		return new Tuple(typeFactory.tupleType(elementTypes), elements);
 	}
 
-	public IValue selectByFieldNames(String... fields){
-		if(fields.length == 1) return get(fields[0]);
+	public IValue selectByFieldNames(String... fields) {
+		if (fields.length == 1)
+			return get(fields[0]);
 
 		int nrOfElements = fields.length;
 		IValue[] elements = new IValue[nrOfElements];
 		Type[] elementTypes = new Type[nrOfElements];
-		for(int i = nrOfElements - 1; i >= 0; i--){
+		for (int i = nrOfElements - 1; i >= 0; i--) {
 			IValue element = get(fields[i]);
 			elements[i] = element;
 			elementTypes[i] = element.getType();
 		}
-		
-		return new Tuple(typeFactory.tupleType(elementTypes),elements);
+
+		return new Tuple(typeFactory.tupleType(elementTypes), elements);
 	}
-	
-	public int hashCode(){
+
+	public int hashCode() {
 		int hash = 1331;
-		
-		for(int i = elements.length - 1; i >= 0; i--){
+
+		for (int i = elements.length - 1; i >= 0; i--) {
 			hash -= (hash << 19) + (hash >>> 8);
 			hash ^= elements[i].hashCode();
 		}
-		
+
 		return hash - (hash << 7);
 	}
 
-	public boolean equals(Object o){
-		if(o == this) return true;
-		if(o == null) return false;
-		
-		if(o.getClass() == getClass()){
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (o == null)
+			return false;
+
+		if (o.getClass() == getClass()) {
 			Tuple otherTuple = (Tuple) o;
-			
+
 			IValue[] otherElements = otherTuple.elements;
 			int nrOfElements = elements.length;
-			if(otherElements.length == nrOfElements){
-				for(int i = nrOfElements - 1; i >= 0; i--){
-					if(!otherElements[i].equals(elements[i])) return false;
+			if (otherElements.length == nrOfElements) {
+				for (int i = nrOfElements - 1; i >= 0; i--) {
+					if (!otherElements[i].equals(elements[i]))
+						return false;
 				}
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
-	public boolean isEqual(IValue value){
-		if(value == this) return true;
-		if(value == null) return false;
-		
-		if(value instanceof Tuple){
+
+	public boolean isEqual(IValue value) {
+		if (value == this)
+			return true;
+		if (value == null)
+			return false;
+
+		if (value instanceof Tuple) {
 			Tuple otherTuple = (Tuple) value;
-			
-			if(!tupleType.comparable(otherTuple.tupleType)) return false;
-			
+
+			if (!tupleType.comparable(otherTuple.tupleType))
+				return false;
+
 			IValue[] otherElements = otherTuple.elements;
 			int nrOfElements = elements.length;
-			if(otherElements.length == nrOfElements){
-				for(int i = nrOfElements - 1; i >= 0; i--){
-					if(!otherElements[i].isEqual(elements[i])) return false;
+			if (otherElements.length == nrOfElements) {
+				for (int i = nrOfElements - 1; i >= 0; i--) {
+					if (!otherElements[i].isEqual(elements[i]))
+						return false;
 				}
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
-	private static class TupleIterator implements Iterator<IValue>{
+
+	private static class TupleIterator implements Iterator<IValue> {
 		private final IValue[] elements;
 		private int index = 0;
-		
-		public TupleIterator(Tuple tuple){
+
+		public TupleIterator(Tuple tuple) {
 			super();
-			
+
 			elements = tuple.elements;
 		}
-		
-		public boolean hasNext(){
+
+		public boolean hasNext() {
 			return index < elements.length;
 		}
-		
-		public IValue next(){
-			if(!hasNext()) throw new NoSuchElementException("No more elements in this iteration.");
-			
+
+		public IValue next() {
+			if (!hasNext())
+				throw new NoSuchElementException("No more elements in this iteration.");
+
 			return elements[index++];
 		}
-		
-		public void remove(){
+
+		public void remove() {
 			throw new UnsupportedOperationException("This iterator doesn't support removal.");
 		}
 	}
+	
 }
