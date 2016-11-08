d0f22b282fa8abfb0921843a9a4d9860fe476b1b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2016-11-08 16:41:19 +0100

    constructor symbols should have labels just like constructor productions

--- src/main/java/org/rascalmpl/value/type/ConstructorType.java
+++ src/main/java/org/rascalmpl/value/type/ConstructorType.java
@@ -71,16 +71,16 @@ import org.rascalmpl.value.type.TypeFactory.TypeReifier;
 				Set<IConstructor> done) {
 			IListWriter w = vf.listWriter();
 
-//			if (type.hasFieldNames()) {
-//				for (int i = 0; i < type.getArity(); i++) {
-//					w.append(symbols().labelSymbol(vf, type.getFieldType(i).asSymbol(vf, store, grammar, done), type.getFieldName(i)));
-//				}
-//			}
-//			else {
+			if (type.hasFieldNames()) {
+				for (int i = 0; i < type.getArity(); i++) {
+					w.append(symbols().labelSymbol(vf, type.getFieldType(i).asSymbol(vf, store, grammar, done), type.getFieldName(i)));
+				}
+			}
+			else {
 				for (Type field : type.getFieldTypes()) {
 					w.append(field.asSymbol(vf, store, grammar, done));
 				}
-//			}
+			}
 
 			IConstructor adt = type.getAbstractDataType().asSymbol(vf, store, grammar, done);
 			IConstructor cons = vf.constructor(getSymbolConstructorType(),  symbols().labelSymbol(vf, adt, type.getName()), w.done());
