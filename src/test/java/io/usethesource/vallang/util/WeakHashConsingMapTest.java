7a3ce48a583cbfee0e2403f35c739b793765b037
Davy Landman <davy.landman@swat.engineering>
2018-06-25 08:58:33 +0200

    Fixed test setup

--- src/test/java/io/usethesource/vallang/util/WeakHashConsingMapTest.java
+++ src/test/java/io/usethesource/vallang/util/WeakHashConsingMapTest.java
@@ -66,10 +66,10 @@ public class WeakHashConsingMapTest {
     @Parameters(name="{0}")
     public static Iterable<ConsingTestParameters> data() {
         List<ConsingTestParameters> result = new ArrayList<>();
-        int[] threads = new int[] { 1, 2, 8 };
-        int[] collisions = new int[] { 1, 2, 4 };
-        //ConsingTestParameters.generate(result, threads, collisions, WeakLockFreeHashConsingTrie::new);
-        //ConsingTestParameters.generate(result, threads, collisions, WeakAlwaysLockingHashConsingMap::new);
+        int[] threads = new int[] { 1, 8 };
+        int[] collisions = new int[] { 1, 4 };
+        ConsingTestParameters.generate(result, threads, collisions, WeakLockFreeHashConsingTrie::new);
+        ConsingTestParameters.generate(result, threads, collisions, WeakAlwaysLockingHashConsingMap::new);
         ConsingTestParameters.generate(result, threads, collisions, WeakWriteLockingHashConsingMap::new);
         ConsingTestParameters.generate(result, threads, collisions, WeakBarelyLockingHashConsingSet::new);
         ConsingTestParameters.generate(result, threads, collisions, WeakLockFreeHashConsingMap::new);
