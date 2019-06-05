7715339268d1f308f1345651de50fad8ec55dfbd
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-06-05 10:48:57 +0200

    added documentation

---
+++ src/main/java/io/usethesource/vallang/impl/persistent/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This contains a full IValueFactory implementation which tries to be as efficient
+ * as possible using persistent data-structures and specializations where possible.
+ */
+package io.usethesource.vallang.impl.persistent;
