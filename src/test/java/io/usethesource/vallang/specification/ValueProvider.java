5b9d23f435f52b7bd2675b3961f300242d2be3d3
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-06-20 16:06:38 +0200

    migrated random number tests

--- src/test/java/io/usethesource/vallang/specification/ValueProvider.java
+++ src/test/java/io/usethesource/vallang/specification/ValueProvider.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
+import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.IInteger;
 import io.usethesource.vallang.IList;
 import io.usethesource.vallang.IMap;
