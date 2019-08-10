10845fed63e307ced9bd4efe31f5ed5952481354
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-08-10 16:59:24 +0200

    removed dead code

--- src/main/java/io/usethesource/vallang/exceptions/IllegalFieldNameException.java
+++ src/main/java/io/usethesource/vallang/exceptions/IllegalFieldNameException.java
@@ -1,16 +1,19 @@
 package io.usethesource.vallang.exceptions;
 
+import org.checkerframework.checker.nullness.qual.EnsuresNonNull;
+
 public class IllegalFieldNameException extends FactTypeDeclarationException {
 	private static final long serialVersionUID = -2480224409679761754L;
 	private int pos;
 	private Object elem;
 
+	@EnsuresNonNull("super.getCause()")
 	public IllegalFieldNameException(int pos, Object elem, ClassCastException cause) {
 		super("Expected a field name at position " + pos + ", but got something different", cause);
 		this.pos = pos;
 		this.elem = elem;
 	}
-
+	
 	public Object getElement() {
 		return elem;
 	}
@@ -22,10 +25,4 @@ public class IllegalFieldNameException extends FactTypeDeclarationException {
 	public synchronized ClassCastException getCause() {
 		return (ClassCastException) super.getCause();
 	}
-
-	public IllegalFieldNameException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	
 }
