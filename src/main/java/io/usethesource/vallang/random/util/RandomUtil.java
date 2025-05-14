29a40e46f53608d48dc1aacaa63b7ba4b9ba1d68
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-05-14 14:57:41 +0200

    removed debug print

--- src/main/java/io/usethesource/vallang/random/util/RandomUtil.java
+++ src/main/java/io/usethesource/vallang/random/util/RandomUtil.java
@@ -191,7 +191,6 @@ public class RandomUtil {
         }
 
         Type constructor = pickRandom(random, candidates);
-        // System.err.println("PICKED:" + constructor);
 
         if (maxDepth <= 0) {
             Type original = constructor;
