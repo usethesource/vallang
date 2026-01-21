4296a02248780152ef7a7103a454b4e873c6923f
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2026-01-21 09:48:16 +0100

    added failing test for issue #308

--- src/test/java/io/usethesource/vallang/basic/RelationSmokeTest.java
+++ src/test/java/io/usethesource/vallang/basic/RelationSmokeTest.java
@@ -446,6 +446,22 @@ public final class RelationSmokeTest {
     }
 
     @ParameterizedTest @ArgumentsSource(ValueProvider.class)
+    public void testSubtractIRelationWithHashCollisions(IValueFactory vf) {
+        var val1 = vf.node("");
+        var val2 = vf.string("\u0000");
+        var val3 = vf.string("990evT");
+        var empty = vf.list();
+        assertTrue(val1.hashCode() == 0 && val1.hashCode() == val2.hashCode()
+            , "these two values should have the same hash code (0) for this test to be effective");
+
+        ISet whole = vf.set(vf.tuple(val1, empty), vf.tuple(val2, empty), vf.tuple(val3, empty));
+
+        assertTrue(whole.delete(vf.tuple(val1, empty)).equals(vf.set(vf.tuple(val2, empty), vf.tuple(val3, empty))));
+        assertTrue(whole.delete(vf.tuple(val2, empty)).equals(vf.set(vf.tuple(val1, empty), vf.tuple(val3, empty))));
+        assertTrue(whole.delete(vf.tuple(val3, empty)).equals(vf.set(vf.tuple(val1, empty), vf.tuple(val2, empty))));
+    }
+
+    @ParameterizedTest @ArgumentsSource(ValueProvider.class)
     public void testSubtractISet(IValueFactory vf) {
         ISet empty1 = vf.set();
         ISet empty2 = vf.set();
